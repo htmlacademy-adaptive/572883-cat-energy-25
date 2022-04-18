4a0305bb28e8cf996e9b3495d48313de0474bd2c
Mutokhlyaev Dmitriy <dmutokhlyaev@yandex.ru>
2022-04-18 21:01:59 +0500

    Добавляет адаптивную вёрстку для блоков Живой пример, Слайдер сравнения и Контакты

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+  /* Выпадающее меню в шапке сайта */
+
   let pageHeader = document.querySelector('.page-header');
   let menuToggler = pageHeader.querySelector('.menu-toggler');
   let headerSiteMenu = pageHeader.querySelector('.site-menu');
@@ -15,5 +17,51 @@ document.addEventListener("DOMContentLoaded", function () {
       menuToggler.classList.add("menu-toggler--opened-menu");
     }
   }
+
+  /* Слайдер срафнения */
+
+  let sliderBeforeItem = document.querySelector(".comparison-slider__item--before");
+  let sliderAfterItem = document.querySelector(".comparison-slider__item--after");
+  let sliderScrollbar = document.querySelector(".comparison-slider__scrollbar");
+  let sliderToggler  = document.querySelector(".comparison-slider__toggler");
+  let sliderBeforeButton = document.querySelector(".comparison-slider__button--before");
+  let sliderAfterButton = document.querySelector(".comparison-slider__button--after");
+
+  let switchToBefore = function () {
+
+    if ( !sliderBeforeItem.classList.contains("comparison-slider__item--active") ) {
+
+      sliderBeforeItem.classList.add("comparison-slider__item--active");
+      sliderAfterItem.classList.remove("comparison-slider__item--active");
+
+      sliderScrollbar.classList.remove("comparison-slider__scrollbar--active");
+
+    }
+
+    sliderBeforeItem.style.width = "100%";
+    sliderAfterItem.style.width = "0";
+    sliderToggler.style.left = "0";
+
+  }
+
+  let switchToAfter = function () {
+
+    if ( !sliderAfterItem.classList.contains("comparison-slider__item--active") ) {
+
+      sliderAfterItem.classList.add("comparison-slider__item--active");
+      sliderBeforeItem.classList.remove("comparison-slider__item--active");
+
+      sliderScrollbar.classList.add("comparison-slider__scrollbar--active");
+
+    }
+
+    sliderBeforeItem.style.width = "0";
+    sliderAfterItem.style.width = "100%";
+    sliderToggler.style.left = "100%";
+
+  }
+
+  sliderBeforeButton.onclick = switchToBefore;
+  sliderAfterButton.onclick = switchToAfter;
 
 });
