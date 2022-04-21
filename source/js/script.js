10a5dd1218e0b95a65c46094de53e774683eb5ba
Mutokhlyaev Dmitriy <dmutokhlyaev@yandex.ru>
2022-04-21 19:01:42 +0500

    Добавляет адаптивную вёрстку для страницы Заявки

--- source/js/script.js
+++ source/js/script.js
@@ -18,71 +18,75 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  /* Слайдер срафнения */
+  /* Слайдер сравнения */
 
-  let sliderBeforeItem = document.querySelector(".comparison-slider__item--before");
-  let sliderAfterItem = document.querySelector(".comparison-slider__item--after");
+  if ( document.body.classList.contains("home") ) {
+
+    let sliderBeforeItem = document.querySelector(".comparison-slider__item--before");
+    let sliderAfterItem = document.querySelector(".comparison-slider__item--after");
 
 
-  let sliderToggler  = document.querySelector(".comparison-slider__toggler");
-  let sliderScroller = document.querySelector(".comparison-slider__scroller");
+    let sliderToggler  = document.querySelector(".comparison-slider__toggler");
+    let sliderScroller = document.querySelector(".comparison-slider__scroller");
 
-  let sliderBeforeButton = document.querySelector(".comparison-slider__button--before");
-  let sliderAfterButton = document.querySelector(".comparison-slider__button--after");
+    let sliderBeforeButton = document.querySelector(".comparison-slider__button--before");
+    let sliderAfterButton = document.querySelector(".comparison-slider__button--after");
 
-  let switchToBefore = function () {
+    let switchToBefore = function () {
 
-    if ( !sliderBeforeItem.classList.contains("comparison-slider__item--active") ) {
+      if ( !sliderBeforeItem.classList.contains("comparison-slider__item--active") ) {
 
-      sliderBeforeItem.classList.add("comparison-slider__item--active");
-      sliderAfterItem.classList.remove("comparison-slider__item--active");
+        sliderBeforeItem.classList.add("comparison-slider__item--active");
+        sliderAfterItem.classList.remove("comparison-slider__item--active");
 
-      sliderToggler.classList.remove("comparison-slider__toggler--active");
+        sliderToggler.classList.remove("comparison-slider__toggler--active");
+
+      }
+
+      sliderBeforeItem.style.width = "100%";
+      sliderAfterItem.style.width = "0";
+      sliderScroller.style.left = "0";
 
     }
 
-    sliderBeforeItem.style.width = "100%";
-    sliderAfterItem.style.width = "0";
-    sliderScroller.style.left = "0";
+    let switchToAfter = function () {
 
-  }
+      if ( !sliderAfterItem.classList.contains("comparison-slider__item--active") ) {
 
-  let switchToAfter = function () {
+        sliderAfterItem.classList.add("comparison-slider__item--active");
+        sliderBeforeItem.classList.remove("comparison-slider__item--active");
 
-    if ( !sliderAfterItem.classList.contains("comparison-slider__item--active") ) {
+        sliderToggler.classList.add("comparison-slider__toggler--active");
+
+      }
+
+      sliderBeforeItem.style.width = "0";
+      sliderAfterItem.style.width = "100%";
+      sliderScroller.style.left = "100%";
+
+    }
+
+    sliderBeforeButton.onclick = switchToBefore;
+    sliderAfterButton.onclick = switchToAfter;
+
+    sliderToggler.onclick = function () {
+      if ( this.classList.contains("comparison-slider__toggler--active") ) {
+        switchToBefore();
+      } else {
+        switchToAfter();
+      }
+    };
+
+    sliderScroller.onclick = function () {
+      sliderBeforeItem.style.width = "50%";
+      sliderAfterItem.style.width = "50%";
+      sliderScroller.style.left = "50%";
 
       sliderAfterItem.classList.add("comparison-slider__item--active");
       sliderBeforeItem.classList.remove("comparison-slider__item--active");
-
-      sliderToggler.classList.add("comparison-slider__toggler--active");
-
+      sliderToggler.classList.remove("comparison-slider__toggler--active");
     }
 
-    sliderBeforeItem.style.width = "0";
-    sliderAfterItem.style.width = "100%";
-    sliderScroller.style.left = "100%";
-
-  }
-
-  sliderBeforeButton.onclick = switchToBefore;
-  sliderAfterButton.onclick = switchToAfter;
-
-  sliderToggler.onclick = function () {
-    if ( this.classList.contains("comparison-slider__toggler--active") ) {
-      switchToBefore();
-    } else {
-      switchToAfter();
-    }
-  };
-
-  sliderScroller.onclick = function () {
-    sliderBeforeItem.style.width = "50%";
-    sliderAfterItem.style.width = "50%";
-    sliderScroller.style.left = "50%";
-
-    sliderAfterItem.classList.add("comparison-slider__item--active");
-    sliderBeforeItem.classList.remove("comparison-slider__item--active");
-    sliderToggler.classList.remove("comparison-slider__toggler--active");
   }
 
 });
